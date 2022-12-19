10b8223fc7d4cc7366a2c0715a41ceb84b774b48
Uchchash346 <mru416@gmail.com>
2022-12-19 10:22:07 +0600

    Set year and sale section

--- src/components/year_and_sale/YearAndSale.js
+++ src/components/year_and_sale/YearAndSale.js
@@ -1,10 +1,28 @@
 import React, { Fragment } from 'react';
-
+import contentImage from '../../assets/content_images/content-image.jpg';
+import contentImageCategory from '../../assets/content_images/content-image-category.jpg';
 const YearAndSale = () => {
     return (
         <Fragment>
-                <h1>This is YearAndSale page</h1>
-            <div className='grid grid-cols-4 gap-4'>
+            <div className='grid grid-cols-2 gap-2 py-3 text-center'>
+                <div>
+                    <img
+                        className='w-full bg-cover bg-center '
+                        src={contentImage} alt="" />
+                </div>
+                <div>
+                    <img
+                        className='w-full bg-cover bg-center '
+                        src={contentImageCategory} alt="" />
+                </div>
+                <div>
+                    <a href="/" className='text-center hover:px-5'>Sale</a>
+                    <a href='/' className='text-2xl px-2'>»</a>
+                </div>
+                <div>
+                    <a href="/" className='text-center hover:px-5'>Waistcoats</a>
+                    <a href='/' className=' text-2xl px-2'>»</a>
+                </div>
             </div>
         </Fragment>
     );
