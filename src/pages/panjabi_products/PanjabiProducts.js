76e2f8abf7c092d725bcf349f7e10a849da7b5f2
Uchchash346 <mru416@gmail.com>
2022-12-20 18:15:16 +0600

    Product Api Setup

---
+++ src/pages/panjabi_products/PanjabiProducts.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const PanjabiProducts = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    );
+};
+
+export default PanjabiProducts;
