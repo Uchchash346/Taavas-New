03733c86309da09b6e56416a92c3faf6769a54f6
Uchchash346 <mru416@gmail.com>
2022-12-21 16:40:53 +0600

    Set sidebar dynamic

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,43 +1,30 @@
-import React, { Fragment } from 'react';
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
 
 const Sidebar = () => {
+    const [sidebar, setSidebar] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("/data/sidebar.json")
+            .then((res) => setSidebar(res.data))
+            .catch((err) => console.log(err))
+
+    }, [])
+
     return (
         <Fragment>
             <div className="w-60 h-auto  pt-5 absolute">
                 <ul className="relative">
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Privacy Policy</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Payment Policy</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Shipping Policy</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Exchange & Return</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Size Guide</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Loyalty Program</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="/terms-conditions" data-mdb-ripple="true" data-mdb-ripple-color="dark">Terms & Conditions</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Display Centers</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">About Us</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">Contact Us</a>
-                    </li>
-                    <li className="relative">
-                        <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href="/intellectual-property" data-mdb-ripple="true" data-mdb-ripple-color="dark">Intellectual Property</a>
-                    </li>
+                    {
+                        sidebar.map((data, index) => {
+                            return (
+                                <li className="relative" key={index}>
+                                    <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href={data.urlLink} data-mdb-ripple="true" data-mdb-ripple-color="dark">{data.name}</a>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         </Fragment>
