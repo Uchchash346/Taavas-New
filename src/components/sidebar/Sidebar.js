ddbe1516a66623c8132c742c574f3bbbc09c2582
Uchchash346 <mru416@gmail.com>
2022-12-21 16:45:44 +0600

    Set sidebar dynamic

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -19,7 +19,7 @@ const Sidebar = () => {
                         sidebar.map((data, index) => {
                             return (
                                 <li className="relative" key={index}>
-                                    <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black transition duration-300 ease-in-out" href={data.urlLink} data-mdb-ripple="true" data-mdb-ripple-color="dark">{data.name}</a>
+                                    <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-black active:bg-black transition duration-300 ease-in-out" href={data.urlLink} data-mdb-ripple="true" data-mdb-ripple-color="dark">{data.name}</a>
                                 </li>
                             )
                         })
