a243ecb179ac2235cdf90494dc7740bf917e9681
Uchchash346 <mru416@gmail.com>
2022-12-21 14:50:31 +0600

    Fixed display center button hover section

--- src/components/display_center/DisplayCenter.js
+++ src/components/display_center/DisplayCenter.js
@@ -7,7 +7,7 @@ const DisplayCenter = () => {
         <Fragment>
             <div className='display-center-section relative text-center'>
                 <img src={DisplayCenterImage} alt="" className='w-full h-96' />
-                <div className='display-center-section absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 w-96 h-16 font-semibold'>
+                <div className='display-center-section-area absolute top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 w-96 h-16 font-semibold'>
                     <a href="/panjabis" className='uppercase tracking-widest inline-block align-middle p-5'>find your display center</a>
                 </div>
             </div>
