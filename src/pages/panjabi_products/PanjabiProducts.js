109d5a6f18dedebfb8f758b3d292580a34600384
Uchchash346 <mru416@gmail.com>
2022-12-21 14:43:23 +0600

    Setup products to the products page

--- src/pages/panjabi_products/PanjabiProducts.js
+++ src/pages/panjabi_products/PanjabiProducts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, Suspense, useEffect, useState } from 'react';
 
 const PanjabiProducts = () => {
     const [panjabi, setPanjabi] = useState([]);
@@ -8,25 +8,27 @@ const PanjabiProducts = () => {
             .get("/data/panjabi_products.json")
             .then((res) => setPanjabi(res.data))
             .catch((err) => console.log(err))
-            
+
     }, [])
     return (
-        <Fragment>
-            <div className="grid grid-cols-5 gap-2">
-                <div>
+        <Suspense fallback={<div>LOADING....</div>}>
+            <Fragment>
+                <div className="grid grid-cols-5 gap-2 pt-16 pb-5 mx-5">
                     {
                         panjabi.map((data, index) => {
                             return (
                                 <div key={index}>
-                                    <img src={data.images[0]} alt="" />
-                                    <h1>{data.id}</h1>
+                                    <img src={data.images[0]} className="hover:`{data.images[1]}`" alt="" />
+                                    <h1 className='text-left'><a href="/panjabis">{data.product_title}</a></h1>
+                                    <p className='text-inherit font-semibold'>Product Code: {data.code}</p>
+                                    <p>BDT: {data.regular_price}</p>
                                 </div>
                             )
                         })
                     }
                 </div>
-            </div>
-        </Fragment>
+            </Fragment>
+        </Suspense>
     );
 };
 
