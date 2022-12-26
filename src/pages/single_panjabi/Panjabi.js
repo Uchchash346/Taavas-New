de3dd7ccbbd5a7d84c1de230b0a8e45496381c93
Uchchash346 <mru416@gmail.com>
2022-12-26 10:08:38 +0600

    Social icon added in the product page

--- src/pages/single_panjabi/Panjabi.js
+++ src/pages/single_panjabi/Panjabi.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,6 +9,12 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper";
+
+// import social icon images
+import facbookIcon from '../../assets/social_icons/facebook.png';
+import messengerIcon from '../../assets/social_icons/messenger.png';
+import whatsappIcon from '../../assets/social_icons/whatsapp.png';
+import twitterIcon from '../../assets/social_icons/twitter.png';
 
 const Panjabi = () => {
     // let { id } = useParams();
@@ -51,6 +57,29 @@ const Panjabi = () => {
                         <button type="" className='relative left-1/2 -translate-x-2/4	border bg-black mt-3 text-white py-2 w-64 uppercase'>Add to Bag</button>
                         <br />
                         <button type="" className='relative left-1/2 -translate-x-2/4 border mt-3 text-black py-2 w-64 uppercase'>Find In Store</button>
+                        <div className='block text-center mt-5' >
+                            <div className='inline-block'>
+                                <a href="https://www.facebook.com/">
+                                    <img src={facbookIcon} className="h-4 w-4 relative m-2" alt="" />
+                                </a>
+                            </div>
+                            <div className='inline-block'>
+                                <a href="https://www.messenger.com/">
+                                    <img src={messengerIcon} className="h-4 w-4 relative m-2" alt="" />
+                                </a>
+                            </div>
+                            <div className='inline-block'>
+                                <a href="https://web.whatsapp.com/">
+                                    <img src={whatsappIcon} className="h-4 w-4 relative m-2" alt="" />
+                                </a>
+
+                            </div>
+                            <div className='inline-block'>
+                                <a href="https://twitter.com/">
+                                    <img src={twitterIcon} className="h-4 w-4 relative m-2" alt="" />
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div >
