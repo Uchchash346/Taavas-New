a71a4535ad8a86209b70fcfe7fdd9a3a1c3de4de
Uchchash346 <mru416@gmail.com>
2022-12-31 18:52:35 +0600

    price filter issues

--- src/components/product-modal/ModalComponent.js
+++ src/components/product-modal/ModalComponent.js
@@ -1,5 +1,7 @@
-import React, { Fragment } from 'react';
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
 import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 
 const customStyles = {
     content: {
@@ -8,6 +10,7 @@ const customStyles = {
         right: 'auto',
         bottom: 'auto',
         marginRight: '-50%',
+        width: '50%',
         transform: 'translate(-50%, -50%)',
     },
 };
@@ -16,6 +19,8 @@ const customStyles = {
 // Modal.setAppElement('#yourAppElement');
 
 const ModalComponent = () => {
+
+    // const [displayPage, setDisplayPage] = useState([]);
 
     let subtitle;
     const [modalIsOpen, setIsOpen] = React.useState(false);
@@ -32,12 +37,45 @@ const ModalComponent = () => {
         setIsOpen(false);
     }
 
+    const [panjabi, setPanjabi] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("/data/panjabi_products.json")
+            .then((res) => setPanjabi(res.data))
+            .catch((err) => console.log(err))
+    }, [])
+
+    const lowToHigh = (a,b) => {
+        const priceLowToHigh =  a.price - b.price;
+        setPanjabi(priceLowToHigh)
+
+        
+    }
+    // const onChange = (e) => {
+    //     const value = e.target.value
+
+    //     switch (value) {
+    //         case "ascending":
+    //             setDisplayPage(displayPage.sort((a, b) => a.label.localeCompare(b.label)))
+    //             break
+    //         case "descending":
+    //             setDisplayPage(displayPage.sort((a, b) => b.label.localeCompare(a.label)))
+    //             break
+    //         case "high-price":
+    //             setDisplayPage(displayPage.sort((a, b) => b.price - a.price))
+    //             break
+    //         case "priceLowToHigh":
+    //             setDisplayPage(displayPage.sort((a, b) => a.price - b.price))
+    //             break
+    //     }
+    // }
 
     return (
         <Fragment>
             <div className='mt-16'>
-                <button onClick={openModal}>Filters & Sort</button>
-                <Modal
+                {/* <button onClick={openModal}>Filters & Sort</button> */}
+                {/* <Modal
                     isOpen={modalIsOpen}
                     onAfterOpen={afterOpenModal}
                     onRequestClose={closeModal}
@@ -49,12 +87,36 @@ const ModalComponent = () => {
                     <div>I am a modal</div>
                     <form>
                         <input />
-                        <button>tab navigation</button>
-                        <button>stays</button>
-                        <button>inside</button>
-                        <button>the modal</button>
-                    </form>
-                </Modal>
+                    </form> */}
+                {
+
+                }
+                <select>
+                    <option value="default">Default</option>
+                    <option value="priceHighToLow">Price - High to Low</option>
+                    <option value="priceLowToHigh" onChange={(e) => lowToHigh(e.target.value)}>Price - Low to High</option>
+                </select>
+                {/* </Modal> */}
+                {/* {
+                    displayPage.map((data, index) => {
+                        const { id, product_title, category, code, color, images, size, description, details, material, care, stock_status, regular_price, sale_price } = data;
+                        return (
+                            <div key={index}>
+                                <Link to={`/panjabis/${data.id}`}
+                                    state={{ id, product_title, category, code, color, images, size, description, details, material, care, stock_status, regular_price, sale_price }}
+                                >
+                                    <img src={data.images[0]}
+                                        onMouseEnter={(e) => e.target.src = data.images[1]}
+                                        onMouseLeave={(e) => e.target.src = data.images[0]}
+                                        alt="" />
+                                    <h1 className='text-left'><a href="/panjabis">{data.product_title}</a></h1>
+                                    <p className='text-inherit font-semibold'>Product Code: {data.code}</p>
+                                    <p>BDT: {data.regular_price}</p>
+                                </Link>
+                            </div>
+                        )
+                    })
+                } */}
             </div>
         </Fragment>
     );
---
+++ src/store/reducers/FilterReducer.js
@@ -0,0 +1,12 @@
+
+// let initialPriceFilter = {};
+
+// const priceFilterReducer = (state = initialPriceFilter,  action) => {
+//     switch(action.type){
+//             case PRICELOWTOHIGH:
+//                 return {
+//                     ...state,
+
+//                 }
+//     }
+// }
